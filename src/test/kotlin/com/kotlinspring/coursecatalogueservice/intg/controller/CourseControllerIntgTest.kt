2acdd14038b439fc4593c5ea4135be69d6163ea5
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2024-01-20 14:47:59 +0100

    Integration test for the POST endpoint to retrieveAllCourses

--- src/test/kotlin/com/kotlinspring/coursecatalogueservice/intg/controller/CourseControllerIntgTest.kt
+++ src/test/kotlin/com/kotlinspring/coursecatalogueservice/intg/controller/CourseControllerIntgTest.kt
@@ -1,7 +1,11 @@
 package com.kotlinspring.coursecatalogueservice.intg.controller
 
 import com.kotlinspring.coursecatalogueservice.dto.CourseDTO
+import com.kotlinspring.coursecatalogueservice.repository.CourseRepository
+import courseEntityList
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
@@ -16,6 +20,17 @@ class CourseControllerIntgTest {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp(){
+        courseRepository.deleteAll()
+
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
 
     @Test
     fun addCourse() {
@@ -36,4 +51,24 @@ class CourseControllerIntgTest {
             savedCourseDTO!!.id != null
         }
     }
+
+    @Test
+    fun retrieveAllCourses() {
+
+        val courseDTOs = webTestClient
+            .get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        println("courseDTOs : $courseDTOs")
+
+        assertEquals(3, courseDTOs!!.size)
+
+    }
+
+
 }
