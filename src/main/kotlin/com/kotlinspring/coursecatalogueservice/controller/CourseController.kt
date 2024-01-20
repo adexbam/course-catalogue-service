45ae57de7d6580702f7ce2d88926d1143142e788
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2024-01-20 14:06:38 +0100

    created a retrieveAllCourses endpoint in the controller

--- src/main/kotlin/com/kotlinspring/coursecatalogueservice/controller/CourseController.kt
+++ src/main/kotlin/com/kotlinspring/coursecatalogueservice/controller/CourseController.kt
@@ -4,19 +4,22 @@ import com.kotlinspring.coursecatalogueservice.dto.CourseDTO
 import com.kotlinspring.coursecatalogueservice.service.CourseService
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/v1/courses")
-class CourseController (val courseService: CourseService){
+class CourseController (val courseService: CourseService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun addCourse(@RequestBody courseDTO: CourseDTO) : CourseDTO {
+    fun addCourse(@RequestBody courseDTO: CourseDTO): CourseDTO {
         return courseService.addCourse(courseDTO)
     }
+
+    @GetMapping
+    fun retrieveAllCourses() {
+        println("courses")
+    }
+
+
 }
