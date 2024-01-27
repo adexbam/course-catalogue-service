2714e562a2bc71e66b9cd9b444453f43ca23ddff
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2024-01-27 20:20:29 +0100

    added Integration test to test multiple set of data using parametarised test

--- src/test/kotlin/com/kotlinspring/coursecatalogueservice/intg/repository/CourseRepositoryIntgTest.kt
+++ src/test/kotlin/com/kotlinspring/coursecatalogueservice/intg/repository/CourseRepositoryIntgTest.kt
@@ -3,10 +3,15 @@ package com.kotlinspring.coursecatalogueservice.intg.repository
 import com.kotlinspring.coursecatalogueservice.repository.CourseRepository
 import courseEntityList
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import java.util.stream.Stream;
 
 @DataJpaTest
 class CourseRepositoryIntgTest {
@@ -38,4 +43,24 @@ class CourseRepositoryIntgTest {
         Assertions.assertEquals(2, courses.size)
     }
 
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize:  Int) {
+
+        val courses = courseRepository.findCoursesByName(name)
+
+        println("courses  : $courses")
+        assertEquals(expectedSize, courses.size)
+
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1))
+
+        }
+    }
 }
